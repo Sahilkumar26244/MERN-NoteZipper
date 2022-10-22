3631547f0b0cd15d1ed829854d00710fa177565d
Sahil Kumar <kumarsahil8981@gmail.com>
2022-10-23 02:26:23 +0530

    day-1

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const notes = require("./data/notes");
+const dotenv = require('dotenv');
+
+
+const app = express();
+dotenv.config();
+
+app.get("/" , (req,res) => {
+    res.send("API is running")
+});
+
+app.get("/api/notes" , (req,res) => {
+    res.json(notes)
+});
+
+app.get("/api/notes/:id" , (req,res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    
+    res.send(note)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,() => {
+    console.log("server is started on 5000")
+})
