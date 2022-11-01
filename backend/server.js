c8127bc53eb21e645cca4000cfa54d7ab426e46e
Sahil Kumar <kumarsahil8981@gmail.com>
2022-11-02 01:07:30 +0530

    Preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const cors = require('cors')
+const path = require('path')
 
 
 
@@ -17,19 +18,25 @@ app.use(express.json());
 
 
 
-
-
-
-app.get("/" , (req,res) => {
-    res.send("API is running")
-});
-
-// app.get("/api/notes" , (req,res) => {
-//     res.json(notes)
-// });
-
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+//---------------------deployment---------------------
+
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"/frontend/build")));
+
+    app.get("*" , (req,res) => {
+        res.sendFile(path.resolve(__dirname , "frontend","build","index.html"));
+    })
+}else{
+    app.get("/" , (req,res) => {
+        res.send("API is running")
+    });
+}
+
+//---------------------deployment---------------------
 
 
 app.use(notFound)
